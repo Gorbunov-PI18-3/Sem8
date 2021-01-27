375d221a7b16529e0587680edb6163f628aed852
Gorbunov-PI18-3 <stfouley5@gmail.com>
2021-01-27 13:09:15 +0300

    Добавьте файлы проекта.

---
+++ Sem8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+
+namespace Sem8
+{
+    class Program
+    {
+        public static void Main()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo("с:\\папка\\");
+            
+            foreach (FileInfo file in dirInfo.GetFiles())
+            {
+            // TimeSpan t = TimeSpan.FromMinutes(30);
+                file.Delete();
+            }
+
+        }
+    }
+}
